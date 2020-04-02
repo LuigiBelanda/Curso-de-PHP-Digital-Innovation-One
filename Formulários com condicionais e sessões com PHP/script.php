d9dd8d8eafa5f90f23b9a61af9778e6a148692b8
LuigiBelanda <luigi.belanda.milani@gmail.com>
2020-04-02 10:55:07 -0300

    adicionando mensagens de sucesso, mais headers etc.

--- Formulários com condicionais e sessões com PHP/script.php
+++ Formulários com condicionais e sessões com PHP/script.php
@@ -29,27 +29,31 @@
     if (empty($nome)){
         $_SESSION['mensagem de erro'] = 'O nome não pode ser vazio, por favor preencha-o novamente';
         header('location: index2.php');
+        return;
     }
     // acima verificamos se o usuário digitou ou não seu nome, caso não tenha digitado seremos redirecionados para a página inicial
 
     // a função strlen() conta a quantidade de caracteres que a string tem, nessa caso o a var $nome
-    if (strlen($nome) < 3){
+    else if (strlen($nome) < 3){
         $_SESSION['mensagem de erro'] = 'O nome não pode conter menos de 3 caracteres';
         header('location: index2.php');
+        return;
     }
     // acima verificamos se o nome colocado pelo usuário é valido, caso não for seremos redirecionados para a página inicial
 
-    if (strlen($nome) > 40){
+    else if (strlen($nome) > 40){
         $_SESSION['mensagem de erro'] = 'O nome não pode ter mais de 40 caracteres';
         header('location: index2.php');
+        return;
     }
     // acima verificamos se o nome do usuário não é muito extenso, no caso se o nome digitado passe de 40 carecteres
     // ele entrara no trecho de codigo acima e nesse caso seremos redirecionados para a página inicial
 
     // a função is_numeric verifica se o dado que estamos recebendo é numerico 
-    if (!is_numeric($idade)){
+    else if (!is_numeric($idade)){
         $_SESSION['mensagem de erro'] = 'Informe um número para idade';
         header('location: index2.php');
+        return;
     }
     // caso a varivel $idade não for numerica (isso por causa do ponto de exclamação antes da função !is_numeric)
     //  ele entrara no trecho de codigo acima e nesse caso seremos redirecionados para a página inicial
@@ -57,7 +61,9 @@
     if ($idade >= 6 && $idade <= 12) { // se idade for maior ou igual a 6 e tambem seja menor ou igual a 12 ele continua esse bloco
        for ($i = 0; $i <= count($categorias); $i++) { 
            if ($categorias[$i] == 'infantil') {
-               echo "O nadador ".$nome. " compete na categoria infantil";
+              $_SESSION['mensagem de sucesso'] = "O nadador ".$nome. " compete na categoria infantil";
+              header ('location: index2.php');
+              return;
            }
        }
     }
@@ -65,7 +71,9 @@
     else if ($idade >= 13 && $idade < 18) {  // se idade for maior ou igual a 13 e tambem seja menor a 18 
         for ($i = 0; $i <= count($categorias); $i++) { 
             if ($categorias[$i] == 'adolescente') {
-                echo "O nadador ".$nome. " compete na categoria adolescente";
+            $_SESSION['mensagem de sucesso'] = "O nadador ".$nome. " compete na categoria adolescente";
+              header ('location: index2.php');
+              return;
             }
         }
     }
@@ -73,11 +81,18 @@
     else { // se nao corresponder a nenhuma das condições acima ele vai fazer o bloco de codigo a seguir
         for ($i = 0; $i <= count($categorias); $i++) { 
             if ($categorias[$i] == 'adulto') {
-                echo "O nadador " .$nome. " compete na categoria adulto";
+                $_SESSION['mensagem de sucesso'] = "O nadador ".$nome. " compete na categoria adulto";
+                header ('location: index2.php');
+                return;
             }
         }
     }
 
     // && = and 
+
+    // return interrompe o código e não executa mais nada do script
+
+    // header mostra para onde queremos redirecionar o usuário, no nosso caso estamos na maioria das
+    // vezes estamos redirecionando para página inicial
     
 ?>
